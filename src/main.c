fa87d355e27f01f93cdb5c214a8139e06143536e
Augusto Fraga Giachero <augustofg96@gmail.com>
2017-09-30 20:09:10 -0300

    Working code 10/10

--- src/main.c
+++ src/main.c
@@ -1,14 +1,31 @@
 #include <stdio.h>
 
-int main() {
-  int a[5];
-  int n;
+int main()
+{
+	int a[5];
+	int n;
 
-  scanf("%d %d %d %d %d %d", &(a[0]), &(a[1]), &(a[2]), &(a[3]), &(a[4]), &n);
+	scanf("%d %d %d %d %d %d", &(a[0]), &(a[1]), &(a[2]), &(a[3]), &(a[4]), &n);
 
-  for (int i=0; i<n; i++) {
-    /* Imprime resultado da n-esima iteracao do scheduler */
-  }
-
-  return 0;
+	for (int i = 1; i <= n; i++)
+	{
+		int exec_task = 0;
+		for (int task = 0; task < 5; task++)
+		{
+			if ((i % a[task]) == 0)
+			{
+				exec_task = 1;
+				printf("%d", task);
+			}
+		}
+		if (exec_task)
+		{
+			printf("\n");
+		}
+		else
+		{
+			printf("-\n");
+		}
+	}
+	return 0;
 }
